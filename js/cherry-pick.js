bc953d11afc91ef4d0136075b54bbe8544799e8e
Jody Zeitler <jody.zeitler@gmail.com>
2014-05-03 11:21:00 -0500

    Modified chart app to use new data structure

--- js/cherry-pick.js
+++ js/cherry-pick.js
@@ -1,112 +1,166 @@
 (function($) {
-	$('#seasonSlider').slider();
-	$('#ratingSlider').slider({
+
+	// init sliders
+	var $seasonSlider = $('#seasonSlider'),
+		$seasonRange  = $('#seasonRange'),
+		$ratingSlider = $('#ratingSlider'),
+		$ratingRange  = $('#ratingRange');
+
+	$seasonSlider.slider();
+	$ratingSlider.slider({
 		range: true,
 		min: 1,
 		max: 10,
 		step: 0.1,
 		values: [1, 10],
 		slide: function(event, ui) {
-			$('#ratingRange').text( ui.values[0].toFixed(1) + ' - ' + ui.values[1].toFixed(1) );
+			$ratingRange.text( ui.values[0].toFixed(1) + ' - ' + ui.values[1].toFixed(1) );
 		},
 		change: function(event, ui) {
 			filterChart();
 		}
 	});
-	$('#ratingRange').text( $('#ratingSlider').slider('values', 0).toFixed(1) + ' - ' + $('#ratingSlider').slider('values', 1).toFixed(1) );
+	$ratingRange.text( $ratingSlider.slider('values', 0).toFixed(1) + ' - ' + $ratingSlider.slider('values', 1).toFixed(1) );
 
 
+	// global data
 	var shows = {},
 		currentShow = null;
 
+	/**
+	 * Initialize the chart using a series object
+	 */
 	function makeChart(data) {
-		var axis = [],
-			series = [],
-			season_label = -255,
-			season_index = -1,
-			latest_season = 1;
+		var axis = [], // x
+			series = [], // y
+			episode_counter = 0,
 
-		for (var i=0; i < data.length; i++) {
-			if (season_label > latest_season) {
-				latest_season = season_label;
-			}
-			if (data[i]['episode_rating'] < $('#ratingSlider').slider('values', 0) || data[i]['episode_rating'] > $('#ratingSlider').slider('values', 1)) {
-				continue;
-			}
-			if (data[i]['season_number'] != season_label) {
-				season_label = data[i]['season_number'];
-				season_index++;
-				axis[season_index] = [];
-				series[season_index] = [];
-			}
-			axis[season_index].push(i);
-			series[season_index].push(data[i]['episode_rating']);
-		};
+			ratingMin = $ratingSlider.slider('values', 0),
+			ratingMax = $ratingSlider.slider('values', 1);
 
-		$('#seasonSlider').slider('destroy');
-		$('#seasonSlider').slider({
+		for (var s=0, season; season = data.seasons[s]; s++) {
+			var season_axis = [],
+				season_series = [];
+
+			for (var e=0, episode; episode = season.episodes[e]; e++, episode_counter++) {
+				if (episode['rating'] < ratingMin || episode['rating'] > ratingMax) {
+					continue;
+				}
+				season_axis.push(episode_counter);
+				season_series.push(episode['rating']);
+			}
+
+			axis.push(season_axis);
+			series.push(season_series);
+		}
+
+		$seasonSlider.slider('destroy');
+		$seasonSlider.slider({
 			range: true,
 			min: 1,
-			max: latest_season,
-			values: [1, latest_season],
+			max: data.season_count,
+			values: [1, data.season_count],
 			slide: function(event, ui) {
-				$('#seasonRange').text( ui.values[0] + ' - ' + ui.values[1] );
+				$seasonRange.text( ui.values[0] + ' - ' + ui.values[1] );
 			},
 			change: function(event, ui) {
 				filterChart();
 			}
 		});
-		$('#seasonRange').text( $('#seasonSlider').slider('values', 0) + ' - ' + $('#seasonSlider').slider('values', 1) );
+		$seasonRange.text( $seasonSlider.slider('values', 0) + ' - ' + $seasonSlider.slider('values', 1) );
 
 		drawChart(axis, series, data);
 	}
 
+	/**
+	 * Filter the current show on the slider values
+	 */
 	function filterChart() {
 		var data = currentShow,
 			axis = [],
 			series = [],
-			season_label = -255,
-			season_index = -1;
+			episode_counter = 0,
 
-		for (var i=0; i < data.length; i++) {
-			if (data[i]['season_number'] < $('#seasonSlider').slider('values', 0) || data[i]['season_number'] > $('#seasonSlider').slider('values', 1)) {
-				continue;
+			seasonMin = $seasonSlider.slider('values', 0),
+			seasonMax = $seasonSlider.slider('values', 1),
+			ratingMin = $ratingSlider.slider('values', 0),
+			ratingMax = $ratingSlider.slider('values', 1);
+
+		for (var s=0, season; season = data.seasons[s]; s++) {
+			var season_axis = [],
+				season_series = [];
+
+			for (var e=0, episode; episode = season.episodes[e]; e++, episode_counter++) {
+				if (episode['season_number'] < seasonMin || episode['season_number'] > seasonMax) {
+					continue;
+				}
+				if (episode['rating'] < ratingMin || episode['rating'] > ratingMax) {
+					continue;
+				}
+				season_axis.push(episode_counter);
+				season_series.push(episode['rating']);
 			}
-			if (data[i]['episode_rating'] < $('#ratingSlider').slider('values', 0) || data[i]['episode_rating'] > $('#ratingSlider').slider('values', 1)) {
-				continue;
-			}
-			if (data[i]['season_number'] != season_label) {
-				season_label = data[i]['season_number'];
-				season_index++;
-				axis[season_index] = [];
-				series[season_index] = [];
-			}
-			axis[season_index].push(i);
-			series[season_index].push(data[i]['episode_rating']);
+
+			axis.push(season_axis);
+			series.push(season_series);
 		}
 
 		drawChart(axis, series, data);
 	}
 
+	/**
+	 * Loop through seasons and get episode by absolute index
+	 */
+	function getEpisodeByIndex(index, data) {
+		data = data || currentShow;
+
+		var counter = 0;
+
+		for (var s=0, season; season = data.seasons[s]; s++) {
+			if (index > counter + season.episodes.length - 1) {
+				counter += season.episodes.length;
+			}
+			else {
+				return season.episodes[index - counter];
+			}
+		}
+	}
+
+	/**
+	 * Draw the chart within the holder element
+	 */
 	function drawChart(axis, series, data) {
-		$('#holder').empty();
+		var $holder = $('#holder').empty();
+
 		var r = Raphael('holder');
-		r.linechart(25, 50, $('#holder').width() - 50, $('#holder').height() - 100,
+
+		r.linechart(25, 50, $holder.width() - 50, $holder.height() - 100,
 			axis,
 			series,
 			{ nostroke: false, symbol: 'circle', smooth: true, axis: '0 0 0 1', axisystep: 10 }
-		).hover(function () {
-			var point = data[this.axis];
-			var msg = 'S' + point['season_number'] + 'E' + point['episode_number'] + '\n' + point['episode_name'] + '\nRating: ' + this.value;
-			this.flag = r.popup(this.x, this.y, msg).insertBefore(this);
-		}, function () {
-			this.flag.animate({opacity: 0}, 300, function () { this.remove(); });
-		}).click(function() {
-			window.open(data[this.axis]['episode_url']);
-		});
+		)
+		.hover(
+			function () {
+				var episode = getEpisodeByIndex(this.axis, data);
+				var msg = 'S' + episode['season_number'] + 'E' + episode['episode_number'] + '\n' + episode['episode_name'] + '\nRating: ' + this.value;
+				this.flag = r.popup(this.x, this.y, msg).insertBefore(this);
+			},
+			function () {
+				this.flag.animate({opacity: 0}, 300, function () { this.remove(); });
+			})
+		.click(
+			function() {
+				var episode = getEpisodeByIndex(this.axis, data);
+				window.open(episode['episode_url']);
+			});
 	}
 
 	$('#showSelect')
+		/**
+		 * Fetch JSON from the server based on option value.
+		 * Set the location hash to option value.
+		 * Initialize and redraw the chart.
+		 */
 		.on('change', function() {
 			var title = $(this).val();
 			window.location.hash = title;
@@ -130,20 +184,28 @@
 		
 	$(window)
 		.on('resize', function() {
-			$('#showSelect').trigger('change');
+			filterChart();
 		})
 		.on('hashchange', function() {
 			$('#showSelect').find('option[value="' + window.location.hash.slice(1) + '"]').prop('selected', true).end().trigger('change');
 		});
 
-	$.get('shows', function(data) {
-		if (data && data.length > 0) {
-			$('#showSelect').empty();
-			for (var i=0; i < data.length; i++) {
-				$('#showSelect').append('<option value="' + data[i] + '">' + data[i].replace(/_/g, ' ') + '</option>');
+	/**
+	 * Try to fetch a list of shows from the server and populate the series options list.
+	 * Select a show based on location hash or the first option and draw the chart.
+	 */
+	$.get('shows')
+		.done(function(data) {
+			if (data && data.length > 0) {
+				$('#showSelect').empty();
+				for (var i=0; i < data.length; i++) {
+					$('#showSelect').append('<option value="' + data[i] + '">' + data[i].replace(/_/g, ' ') + '</option>');
+				}
 			}
-			$('#showSelect').find('option[value="' + window.location.hash.slice(1) + '"]').prop('selected', true).end().trigger('change');
-		}
-	});
+		})
+		.always(function() {
+			$('#showSelect').find('option[value="' + window.location.hash.slice(1) + '"]').prop('selected', true);
+			$('#showSelect').trigger('change');
+		});
 
 })(jQuery);
